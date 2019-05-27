f1dd92d2429636dd0c4a90151241df8cfc0ddab0
inpyo hong <honginpyo1@hanmail.net>
2019-05-27 23:30:32 +0900

    read text

--- sample.sh
+++ sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Program name: sample.sh
 date
-cat /root/1.txt |  while read output
+while read IP
 do
-    mtr -r -c 10 "$output" --csv > /mnt/s3/result.csv
-done
+    mtr "${IP}" -r -c 10 --csv > /mnt/s3/result.csv
+done < /root/sample.sh
