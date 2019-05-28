612f311b3bab5e6a0d92ae3a3ee4333bc6bb1e02
inpyo hong <honginpyo1@hanmail.net>
2019-05-28 15:26:39 +0900

    file name

--- sample_AIC.sh
+++ sample_AIC.sh
@@ -3,6 +3,6 @@
 HOSTNAME=`hostname -f`
 while read IP
 do
-    echo "\n${IP}" >> /mnt/s3/result.csv
+    echo "\n${IP}" >> /mnt/s3/${HOSTNAME}_result.csv
     mtr "${IP}" -w -r -b -c 100 | tail -2 >> /mnt/s3/${HOSTNAME}_result.csv
 done < /root/AIC
