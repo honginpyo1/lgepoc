7f0ffad4cd1752043cbb2a3cb3c6fcc7a4937529
inpyo hong <honginpyo1@hanmail.net>
2019-05-28 15:27:42 +0900

    hostname

--- sample_APIC.sh
+++ sample_APIC.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Program name: sample.sh
+HOSTNAME=`hostname -f`
 while read IP
 do
-    echo "\n${IP}" >> /mnt/s3/result.csv
-    mtr "${IP}" -w -r -b -c 100 | tail -2 >> /mnt/s3/result.csv
+    echo "\n${IP}" >> /mnt/s3/${HOSTNAME}_result.csv
+    mtr "${IP}" -w -r -b -c 100 | tail -2 >> /mnt/s3/${HOSTNAME}_result.csv
 done < /root/APIC
