2620d6b1f3d04e928c5d3dd1262dbf68ac1de9f3
inpyo hong <honginpyo1@hanmail.net>
2019-05-30 09:57:33 +0900

    ignore

---
+++ credentials.tf
@@ -0,0 +1,24 @@
+variable "ibm_bmx_api_key" {
+  type = "string"
+  default = "c1298add565a924d97294b814f8441753db30dd860e43c293134cc71b36d1b90"
+}
+
+variable "ibm_sl_username" {
+  type = "string"
+  default = "IBM1557043"
+}
+
+variable "ibm_sl_api_key" {
+  type = "string"
+  default = "c1298add565a924d97294b814f8441753db30dd860e43c293134cc71b36d1b90"
+}
+
+variable "vm_count" {
+  type = "string"
+  default = "1"
+}
+
+variable "lg_ips_us" {
+  type = "string"
+  default = "8.8.8.8"
+}
